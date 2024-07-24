31790a23673db4ebb60638d94b9f1221e91bb163
Salman Shaikh <salman.shaikh1678@gmail.com>
2024-07-23 20:44:02 -0400

    commit project

---
+++ RadosGWClientLibrary/Models/RadosStorageObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadosGWClientLibrary.Models
+{
+    public class RadosStorageObject
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string UserId { get; set; }
+        public string Version { get; set; }
+        public DateTime LastModified { get; set; }
+        public KeyValuePair<string, string> MetaData { get; set; }
+    }
+}
